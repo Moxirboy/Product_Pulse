a68de2a00a03820bf1c144999cdfc29a6c257662
moxirboy <Moxirboy.M@iman.ux>
2023-11-23 14:41:31 +0500

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.java_project {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.java_project to javafx.fxml;
+    exports com.example.java_project;
+}
